d1b08cc1a149540b151921ad0be4a6bd805466d7
elleelisa <elleelisa@users.noreply.github.com>
2017-06-13 14:46:50 +0300

    skoori osa

--- home.js
+++ home.js
@@ -33,11 +33,45 @@ if (localStorage.getItem("currentPlayer") !== "") {
   localStorage.setItem("allScores", JSON.stringify(highScores));
 }
 
-function populateTable() {
+/*function populateTable() {
   for (var cnt = 0; cnt < highScores.length; cnt++)
   document.getElementById("ScoreData").innerHTML += "<tr><td>" + highScores[cnt].name + "</td><td>" + highScores[cnt].hscore + "</td><td></td></tr>";
 
+}*/
+/*function populateTable(){
+  var table = "<table>";
+
+  	for (var i=0; i< highScores.length; i++) {
+  		table = table + "<tr><td>"+ highScores[i].name+"</td><td>"+ highScores[i].hscore+"</td><td>"+ highScores[i].gwords +"</td></tr>";
+  	}
+  table = table + "</table>";
+
+  document.getElementById('ScoreData').innerHTML = table;
+}*/
+
+function populateTable() {
+  var result = "";
+  for (var cnt = 0; cnt < highScores.length; cnt++) {
+    result = result + "<br>Nimi: " + highScores[cnt].name + "<br>Skoor: " + highScores[cnt].hscore + "<br>Sõnad: " + highScores[cnt].gwords;
+    document.getElementById("scoreList").innerHTML = result;
+  }
 }
+
+/*function populateTable() {
+  results = "";
+  results = "<table>";
+    for (var cnt = 0; cnt < highScores.length; cnt=cnt+2) {
+    results += "<tr><td>" + highScores[cnt].name + "</td>";
+      if(cnt+1 < highScores.length){
+        results += "<td>" + highScores[cnt+1].hscore + "</td></tr>";
+      } else {
+        results += "<td></td></tr>";
+      }
+    }
+  results += "<table><br /><br />";
+  var div = document.getElementById("ScoreData");
+  div.innerHTML = results;
+}*/
 
 function getName(arrayID) {
   return highScores[arrayID].name;
